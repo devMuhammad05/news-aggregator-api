a6cf15b010eba47b396e78fada59a79ff00a83a3
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2025-11-22 21:20:18 +0100

    add: migrations

---
+++ database/migrations/2025_11_22_066237_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('author_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->string('description')->nullable();
+            $table->longText('content');
+            $table->string('source');
+            $table->string('source_url')->nullable();
+            $table->text('image_url')->nullable();
+            $table->dateTime('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
