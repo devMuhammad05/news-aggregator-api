8304febe1e991a82f576ed60f18e0855fcc4bc62
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2025-11-22 07:15:57 +0100

    chore: Add Rector and Rector Laravel packages with initial configuration.

---
+++ rector.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use RectorLaravel\Set\LaravelSetList;
+use RectorLaravel\Set\LaravelLevelSetList;
+use RectorLaravel\Rector\ClassMethod\AddGenericReturnTypeToRelationsRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+
+return RectorConfig::configure()
+
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap',
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])
+
+    ->withSets([
+        SetList::PHP_82,
+        SetList::PHP_83,
+        SetList::CODING_STYLE,
+        LaravelLevelSetList::UP_TO_LARAVEL_120,
+        LaravelSetList::LARAVEL_TYPE_DECLARATIONS,
+        LaravelSetList::LARAVEL_IF_HELPERS,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+        LaravelSetList::LARAVEL_STATIC_TO_INJECTION,
+        LaravelSetList::LARAVEL_TESTING,
+        LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL
+    ])
+
+    ->withRules([
+        DeclareStrictTypesRector::class,
+    ])
+
+    ->withSkip([
+        __DIR__ . '/bootstrap/cache',
+    ]);
